66c75b2917a116df0f65625f21da80f7f0df3dfb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-21 20:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+function bumbCounter() {
+  var counter = 0 ;
+  function addBump() {
+    counter += 1
+  }
+  function getBumps() {
+    return counter
+  }
+  return {addBump, getBumps}
+}
